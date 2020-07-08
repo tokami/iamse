e411b8a2b57182552fbf100f68651c3f8a616261
tokami <tobm@aqua.dtu.dk>
2020-07-08 15:14:13 +0200

    multiple indices

--- R/obsmod.R
+++ R/obsmod.R
@@ -24,33 +24,50 @@ obsmod <- function(specdat, hist, set, years = NULL){
     if(is.null(eC)) eC <- rnorm(ny, 0, set$CVC) - set$CVC^2/2
     if(is.null(eI)) eI <- rnorm(ny, 0, set$CVI) - set$CVI^2/2
 
+    nsurv <- length(set$surveyTimes)
+    if(length(q) < nsurv) q <- rep(q,nsurv)
+
     ## adding observation
     if("obsC" %in% names(hist$obs)){
         y <- length(hist$TSB)
         tmp1 <- length(hist$obs$obsC)
         tmp <- y - tmp1
         if(tmp > 0){
+            ## add catch obs
             obsC <- c(hist$obs$obsC, exp(log(CW[y]) + eC[y-specdat$ny]))
-            obsI <- list()
-            obsI[[1]] <- c(hist$obs$obsI[[1]], exp(log(q[1]) + log(TSB[y]) + eI[y-specdat$ny]))
             timeC <- c(hist$obs$timeC, tail(hist$obs$timeC,1)+1)
-            timeI <- list()
-            timeI[[1]] <- c(hist$obs$timeI[[1]], tail(hist$obs$timeI[[1]],1)+1)
-            sdI <- list()
-            sdI[[1]] <- c(hist$obs$obsIsd[[1]], set$CVI)
+            ## add survey obs
+            Z <- hist$FAA[y,] + dat$M
+            obsI <- vector("list", nsurv)
+            timeI <- vector("list", nsurv)
+            for(i in 1:nsurv){
+                naa <- exp(log(hist$NAA[y,]) - Z * set$surveyTimes[i])
+                if(inherits(naa, "matrix")){
+                    tsb <- apply(naa, 1, function(x) sum(x * dat$weight))
+                }else{
+                    tsb <- sum(naa * dat$weight)
+                }
+                obsI[[i]] <- c(hist$obs$obsI[[i]], exp(log(q[i]) + log(tsb[idx]) + eI[y-specdat$ny]))
+                timeI[[i]] <- c(hist$obs$timeI[[i]], tail(hist$obs$timeI[[i]],1) + 1 + set$surveyTimes[i])
+                ## sdI[[1]] <- c(hist$obs$obsIsd[[i]], set$CVI)
+            }
         }else return(hist)
     ## initalizing observations
     }else{
         ## catch
         obsC <- exp(log(CW[idx]) + eC)
         timeC <- 1:ny
-        ## index time 1
-        obsI1 <- exp(log(q[1]) + log(TSB[idx]) + eI)
-        obsI <- list(obsI1)
-        timeI1 <- 1:ny
-        timeI <- list(timeI1)
-        sdI1 <- rep(set$CVI, length(obsI1))
-        sdI <- list(sdI1)
+        ## survey indices
+        obsI <- vector("list", nsurv)
+        timeI <- vector("list", nsurv)
+        Z <- hist$FAA[idx,] + dat$M
+        for(i in 1:nsurv){
+            naa <- exp(log(hist$NAA[idx,]) - Z * set$surveyTimes[i])
+            tsb <- apply(naa, 1, function(x) sum(x * dat$weight))
+            obsI[[i]] <- exp(log(q[i]) + log(tsb) + eI)
+            timeI[[i]] <- 1:ny + set$surveyTimes[i]
+            ## sdI[[i]] <- rep(set$CVI, length(obsI1))
+        }
     }
 
     ## output indices
@@ -60,7 +77,7 @@ obsmod <- function(specdat, hist, set, years = NULL){
     obs$timeC <- timeC
     obs$obsI <- obsI
     obs$timeI <- timeI
-    obs$obsIsd <- sdI
+    ## obs$obsIsd <- sdI
     out$obs <- obs
     if("refs" %in% names(hist)) out$refs <- hist$refs
     return(out)
