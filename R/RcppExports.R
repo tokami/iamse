c491101b8c8509765fd499d78df1e4f47e1670e9
tokami <t.k.mildenberger@gmail.com>
2025-11-19 15:26:52 +0100

    clean-up

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -25,3 +25,4 @@ initdist <- function(MAA, FAA, R0, spawning, indage0) {
 simpop <- function(logFM, dat, set, out) {
     .Call('_iamse_simpop', PACKAGE = 'iamse', logFM, dat, set, out)
 }
+
---
+++ R/iamse-package.R
@@ -0,0 +1,8 @@
+#' iamse: Intra-annual management strategy evaluation (IAMSE)
+#'
+#' Contains framework for intra-annual age-based operating model for MSE.
+#'
+#' @docType package
+#' @name iamse
+#' @keywords internal
+"_PACKAGE"
